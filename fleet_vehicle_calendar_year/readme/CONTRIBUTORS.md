284de8cd403f8033a15dda5197057a9a2611b383
elheit <meslamineelheit@gmail.com>
2024-11-05 04:05:47 +0000

    [17.0][MIG] fleet_vehicle_calendar_year: Migration to 17.0

---
+++ fleet_vehicle_calendar_year/readme/CONTRIBUTORS.md
@@ -0,0 +1,3 @@
+- Marcel Savegnago \<<marcel.savegnago@escodoo.com.br>\>
+- Kaynnan Lemes \<<kaynnan.lemes@escodoo.com.br>\>
+- Elheit Med Lamine \<<meslamineelheit@gmail.com>\>
---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-fleet",
+    description="Meta package for oca-fleet Odoo addons",
+    version=version,
+    install_requires=[
+	    'odoo-addon-fleet_vehicle_calendar_year>=17.0dev,<17.1dev',
+        'odoo-addon-fleet_vehicle_usage>=17.0dev,<17.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 17.0',
+    ]
+)
