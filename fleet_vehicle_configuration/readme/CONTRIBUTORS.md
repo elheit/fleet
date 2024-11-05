9834be732c00b9fe2d720a2669bcc6112b70735b
elheit <meslamineelheit@gmail.com>
2024-11-05 04:20:30 +0000

    [17.0][MIG] fleet_vehicle_configuration: Migration to 17.0

---
+++ fleet_vehicle_configuration/readme/CONTRIBUTORS.md
@@ -0,0 +1,3 @@
+- Camptocamp SA:
+  - Damien Crier \<<damien.crier@camptocamp.com>\>
+  - Camille Morand \<<camille.morand@camptocamp.com>\>
---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo-addons-oca-fleet",
+    description="Meta package for oca-fleet Odoo addons",
+    version=version,
+    install_requires=[
+	    'odoo-addon-fleet_vehicle_configuration>=17.0dev,<17.1dev',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 17.0',
+    ]
+)
